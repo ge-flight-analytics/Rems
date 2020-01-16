0354133e6d50452ab88d0dbef7ebb3d67385c2a0
Daniel Verkamp <daniel.verkamp@ge.com>
2020-01-16 09:57:50 -0600

    updated db_create.R, should have pushed a long time ag.

--- R/db_create.R
+++ R/db_create.R
@@ -1,12 +1,3 @@
-basic_ops <- list(
-  "==" = 'equal', "!=" = 'notEqual', "<" = 'lessThan',
-  "<=" = 'lessThanOrEqual', ">" = 'greaterThan', ">=" = 'greaterThanOrEqual'
-)
-sp_ops <- list(
-  ' in '= 'in',
-  ' not in '= 'notIn'
-)
-
 #' @export
 insert_query <-
   function(conn, ems_name, db_id, data_file=NULL)
@@ -18,12 +9,12 @@ insert_query <-
     obj$ems        <- ems(conn)
     obj$ems_id     <- get_id(obj$ems, ems_name)
     obj$db_id      <- db_id
-    
+
     # object data
     obj$create <- list(createColumns=list())
     obj$rows <- 0
     obj <- reset(obj)
-    
+
     return(obj)
   }
 
@@ -31,9 +22,9 @@ insert_query <-
 insert_row.InsertQuery <-
   function(qry, row)
   {
-    #' 
+    #'
     #' Inputs:
-    #'    row (list): A list of values to input, where the key is the fieldId and the value is the value to input. 
+    #'    row (list): A list of values to input, where the key is the fieldId and the value is the value to input.
     #'        e.g. row <- list(fieldId1 = value1, fieldId2 = value2, ..., fieldIdN = valueN)
     curr_length <- length(qry$create$createColumns) # Get length of createColumns field
     next_entry <- curr_length + 1 # Find next entry, which is where we want to put our next row
@@ -50,21 +41,24 @@ insert_row.InsertQuery <-
 
 #' @export
 insert_data_frame.InsertQuery <-
-  function(qry, df, schema_map)
+  function(qry, df, schema_map=NULL)
   {
     #'
     #' Inputs:
     #'    df (data.frame): A DataFrame of values to input, where the columns are the fieldIds and the entries are values to input.
-    #'    mapper (list): A mapping of named dataframe columns to field ids, e.g. list('column1' = '[-hub][schema]')
-    
-    # make sure that all columns in df are also in mapper, which maps df column names to schemas
-    for (col in colnames(df)){
-      if (!(col %in% names(schema_map))){
-        cat(sprintf("Column: '%s' found in df, but not in mapper.  Please only pass in columns which should be updated in the target table and for which a
-                    schema mapping exists in the supplied mapper list.", col))
+    #'    schema_map (list): A mapping of named dataframe columns to field ids, e.g. list('column1' = '[-hub][schema]')
+
+    # if schema_map is not null, we want to translate column names to fields in EMS, so we need to make sure all of the dataframe
+    # columns are also in schema_map, which maps df column names to schemas in EMS
+    if (!is.null(schema_map)){
+      for (col in colnames(df)){
+        if (!(col %in% names(schema_map))){
+          cat(sprintf("Column: '%s' found in df, but not in mapper.  Please only pass in columns which should be updated in the target table and for which a
+                      schema mapping exists in the supplied mapper list.", col))
+        }
       }
     }
-    
+
     print('inserting')
     i <- 1 # Begin to loop over all key:value pairs in row.
     for (i in 1:nrow(df)){
@@ -75,7 +69,12 @@ insert_data_frame.InsertQuery <-
       j <- 1
       for (name in names(row)){
         # Add all keys and values as {fieldId = key, value = value}
-        qry$create$createColumns[[next_entry]][[j]] <- list(fieldId = schema_map[[name]], value = row[[name]])
+        # Use schema_map to translate dataframe column names into a schema, if schema_map is not null
+        if (!is.null(schema_map)){
+          qry$create$createColumns[[next_entry]][[j]] <- list(fieldId = schema_map[[name]], value = row[[name]])
+        } else { # if schema_map is not null, assume column names are schemas
+          qry$create$createColumns[[next_entry]][[j]] <- list(fieldId = name, value = row[[name]])
+        }
         j <- j + 1
       }
       i <- i + 1
@@ -101,9 +100,10 @@ run.InsertQuery <-
       }
     } else if (status_code(r) %in% c(400, 401, 503)) {
       cat(print('Failed to add rows.'))
-      cat(sprintf('message: %s'))
-      cat(sprintf('messageDetail: %s'))
-      cat(sprintf('unexpected: %s'))
+      content <- content(r)
+      cat(sprintf('message: %s', content$message))
+      cat(sprintf('messageDetail: %s', content$messageDetail))
+      cat(sprintf('messageDetail: %s', content$unexpected))
       return(FALSE)
     } else {
       cat(print("An unknown error occured."))
@@ -128,64 +128,4 @@ json_str <-
   function(qry)
   {
     jsonlite::toJSON(qry$create, auto_unbox = T, pretty = T)
-  }
-
-
-#' @export
-run.FltQuery <-
-  function(qry, n_row = 25000)
-  {
-    if (is.null(qry$queryset$top)) {
-      Nout <- NULL
-    } else {
-      Nout <- qry$queryset$top
-    }
-    if ((!is.null(Nout)) && (Nout <= 25000)) {
-      return(simple_run(qry, output = "dataframe"))
-    }
-    return(async_run(qry, n_row = n_row))
-  }
-
-## --------------------------------------------------------------------------
-## Functions to interface with the flight object
-
-#' @export
-update_dbtree <-
-  function(qry, ...)
-  {
-    path <- unlist(list(...))
-    qry$flight <- update_tree(qry$flight, path, treetype='dbtree')
-    qry
-  }
-
-#' @export
-update_fieldtree <-
-  function(qry, ...)
-  {
-    path <- unlist(list(...))
-    qry$flight <- update_tree(qry$flight, path, treetype='fieldtree')
-    return(qry)
-  }
-
-#' @export
-generate_preset_fieldtree <-
-  function(qry)
-  {
-    qry$flight <- make_default_tree(qry$flight)
-    qry
-  }
-
-#' @export
-save_metadata <-
-  function(qry, file_name = NULL)
-  {
-    save_tree(qry$flight, file_name)
-  }
-
-#' @export
-load_metadata <-
-  function(qry, file_name = NULL)
-  {
-    qry$flight <- load_tree(qry$flight, file_name)
-    return(qry)
   }
